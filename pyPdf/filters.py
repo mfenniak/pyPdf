93699c7caba05da8391d8b5f6557ccc5a9b01f66
mfenniak <devnull@localhost>
2006-06-05 08:00:36 -0600

    [svn] Add working and tested .NET framework based code for compression and decompression.

--- pyPdf/filters.py
+++ pyPdf/filters.py
@@ -43,28 +43,53 @@ try:
     def compress(data):
         return zlib.compress(data)
 except ImportError:
-    # Unable to import zlib.  If running in IronPython, try using
-    # System.IO.Compression instead.
-    from System import IO, Text
-    _encoding = Text.ASCIIEncoding()
-    def _string_to_bytearr(string):
-        return _encoding.GetBytes(string)
+    # Unable to import zlib.  Attempt to use the System.IO.Compression
+    # library from the .NET framework. (IronPython only)
+    import System
+    from System import IO, Collections, Array
+    def _string_to_bytearr(buf):
+        retval = Array.CreateInstance(System.Byte, len(buf))
+        for i in range(len(buf)):
+            retval[i] = ord(buf[i])
+        return retval
     def _bytearr_to_string(bytes):
-        return _encoding.GetString(bytes)
-    def _read_bytes(gz, byteArray):
-        pass
+        retval = ""
+        for i in range(bytes.Length):
+            retval += chr(bytes[i])
+        return retval
+    def _read_bytes(stream):
+        ms = IO.MemoryStream()
+        buf = Array.CreateInstance(System.Byte, 2048)
+        while True:
+            bytes = stream.Read(buf, 0, buf.Length)
+            if bytes == 0:
+                break
+            else:
+                ms.Write(buf, 0, bytes)
+        retval = ms.ToArray()
+        ms.Close()
+        return retval
     def decompress(data):
         bytes = _string_to_bytearr(data)
         ms = IO.MemoryStream()
         ms.Write(bytes, 0, bytes.Length)
         ms.Position = 0  # fseek 0
-        gz = IO.Compression.GZipStream(ms, IO.Compression.CompressionMode.Decompress)
+        gz = IO.Compression.DeflateStream(ms, IO.Compression.CompressionMode.Decompress)
         bytes = _read_bytes(gz)
         retval = _bytearr_to_string(bytes)
         gz.Close()
         return retval
-    # implementation not complete or tested, but checked in just for fun.
-    
+    def compress(data):
+        bytes = _string_to_bytearr(data)
+        ms = IO.MemoryStream()
+        gz = IO.Compression.DeflateStream(ms, IO.Compression.CompressionMode.Compress, True)
+        gz.Write(bytes, 0, bytes.Length)
+        gz.Close()
+        ms.Position = 0 # fseek 0
+        bytes = ms.ToArray()
+        retval = _bytearr_to_string(bytes)
+        ms.Close()
+        return retval
 
 
 class FlateDecode(object):
