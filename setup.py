7645f9e07b663ba253105fda99be3042c3293afa
mfenniak <devnull@localhost>
2006-12-10 17:46:02 -0700

    [svn] update vers number and changelog for vers 1.7

--- setup.py
+++ setup.py
@@ -19,13 +19,13 @@ It is therefore a useful tool for websites that manage or manipulate PDFs.
 
 setup(
         name="pyPdf",
-        version="1.6",
+        version="1.7",
         description="PDF toolkit",
         long_description=long_description,
         author="Mathieu Fenniak",
         author_email="mfenniak@pobox.com",
         url="http://pybrary.net/pyPdf/",
-        download_url="http://pybrary.net/pyPdf/pyPdf-1.6.tar.gz",
+        download_url="http://pybrary.net/pyPdf/pyPdf-1.7.tar.gz",
         classifiers = [
             "Development Status :: 4 - Beta",
             "Intended Audience :: Developers",
