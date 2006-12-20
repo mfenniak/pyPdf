fa56888b69d3586f191a156c1ebcf0e4ccd7b61f
mfenniak <devnull@localhost>
2006-12-20 14:18:57 -0700

    [svn] Delay recursive update, fixes bug in IronPython usage.

--- pyPdf/filters.py
+++ pyPdf/filters.py
@@ -34,7 +34,6 @@ Implementation of stream filters for PDF.
 __author__ = "Mathieu Fenniak"
 __author_email__ = "mfenniak@pobox.com"
 
-from generic import NameObject
 
 try:
     import zlib
@@ -208,6 +207,7 @@ class ASCII85Decode(object):
     decode = staticmethod(decode)
 
 def decodeStreamData(stream):
+    from generic import NameObject
     filters = stream.get("/Filter", ())
     if len(filters) and not isinstance(filters[0], NameObject):
         # we have a single filter instance
